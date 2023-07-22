8e797df576f922003abb2a98d31f8f186221de50
marclnomaia <maronline35@gmail.com>
2023-07-22 15:52:16 -0400

    first commit

---
+++ rand-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import RandBar from './components/RandBar';
+
+function App() {
+  return (
+    <div className="App">
+      <RandBar/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ rand-app/src/components/RandBar.tsx
@@ -0,0 +1,18 @@
+
+  //import * as React from 'react';
+  import AppBar from '@mui/material/AppBar';
+  import Toolbar from '@mui/material/Toolbar';
+  import Typography from '@mui/material/Typography';
+  
+  export const RandBar = () => {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My App Title</Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  };
+  
+  export default RandBar;
+  
