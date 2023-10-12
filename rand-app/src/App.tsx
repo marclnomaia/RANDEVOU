d09b082cf720a46326eb17e623684008121133db
marclnomaia <maronline35@gmail.com>
2023-10-12 14:20:52 -0400

    7-grid_commit

--- rand-app/src/App.tsx
+++ rand-app/src/App.tsx
@@ -1,68 +1,18 @@
-
-
-
-//import { Grid } from '@mui/material';
 import './App.css';
 import RandNavBar from './components/Header/RandNavBar';
 import RandExperience from './components/Body/Profile/RandExperience';
-//import RandRating from './components/Sidebar/RandRating';
 import RandTaskBar from './components/Body/Personal_Information/RandTaskBar';
-//import { blue } from '@mui/material/colors';
 import RandInfo from './components/Body/Head_Info/RandInfo';
-//import { GridTest1 } from './components/GridEx/GridTest1';
-//import { GridScratch } from './components/GridEx/GridScratch';
 import { Box, Grid } from '@mui/material';
-
-//import { RandInfo } from './components/Personal_Info/RandInfo';
-
-
-//import NavBar from './components/GridEx/NavBar';
-//import Bar from './components/GridEx/Bar';
-//import TaskBar from './components/GridEx/TaskBar';
-//import Profile from './components/GridEx/profile';
-//import RandNav from './components/GridEx/RandNav';
-
-//import { GridScratch } from './components/GridEx/GridScratch';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Randevou";
+  }, []);
+
   return (
     <div>
-      {/*} <div className="App">
-
-      <RandNavBar />
-      <RandProfile />
-      <RandRating />
-      <RandTaskBar />
-      
-      <Profile />
-      <TaskBar />
-       <RandNavBar />
-      <GridTest1 />
-      <GridScratch />
-      <Bar />
-      <NavBar />
-  </div>*/}
-      {/*}
-      <Grid container spacing={2}>
-
-        <Grid item xs={12} sm={6} md={4} lg={4} >
-          <RandProfile />
-        </Grid>
-        <Grid item xs={12} sm={6} md={8} lg={8}>
-          <RandInfo />
-        </Grid>
-        <Grid item xs={12} sm={6} md={8} lg={8}>
-          <RandTaskBar />
-        </Grid>
-      </Grid>
-
-
-      <RandProfile />
-
-      <RandTaskBar />
-
-      <RandInfo />
-*/}
 
       <Grid item xs={12} sm={9} md={12}>
         <Grid container spacing={1}>
@@ -91,16 +41,11 @@ function App() {
                     <RandTaskBar />
                   </Box>
                 </Grid>
-
-
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
-
-
-
     </div >
 
 
