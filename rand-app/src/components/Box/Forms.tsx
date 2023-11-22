6494b8a06f369136abb6197c432c78221beb0573
marclnomaia <maronline35@gmail.com>
2023-11-22 15:52:58 -0500

    2-box_commit

--- rand-app/src/components/Box/Forms.tsx
+++ rand-app/src/components/Box/Forms.tsx
@@ -10,20 +10,54 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
 
 export default function Forms() {
   const [text, setText] = React.useState('');
-  const [numero, setNumero] = React.useState(0);
-  const [show] = React.useState(true);
-  const [rows, setRows] = React.useState<{ text: string; numero: number; }[]>([]);
+  const [numbers, setNumbers] = React.useState(0);
+  const [rows, setRows] = React.useState<{ text: string; numbers: number }[]>([]);
+  const [editIdx, setEditIdx] = React.useState(-1);
+  const [editText, setEditText] = React.useState('');
+  const [editNumbers, setEditNumbers] = React.useState(0);
 
-  const onClickButao = () => {
-    setRows(old => [...old, { text, numero }]);
+  const handleAdd = () => {
+    setRows(old => [...old, { text, numbers }]);
+    setText('');
+    setNumbers(0);
+  };
+
+  const startEdit = (idx: React.SetStateAction<number>, row: { text: any; numbers: any; }) => {
+    setEditIdx(idx);
+    setEditText(row.text);
+    setEditNumbers(row.numbers);
+  };
+
+  const cancelEdit = () => {
+    setEditIdx(-1);
+    setEditText('');
+    setEditNumbers(0);
+  };
+
+  const saveEdit = (idx: number) => {
+    const updatedRows = rows.map((row, index) =>
+      index === idx ? { ...row, text: editText, numbers: editNumbers } : row
+    );
+    setRows(updatedRows);
+    cancelEdit();
+  };
+
+  const deleteRow = (idx: number) => {
+    const updatedRows = rows.filter((_, index) => index !== idx);
+    setRows(updatedRows);
   };
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={6} lg={4}>
+      <Grid item xs={12} md={8} lg={9}>
         <Box
           component="form"
           sx={{
@@ -47,20 +81,15 @@ export default function Forms() {
               id="outlined-number"
               label="Number"
               type="number"
-              value={numero}
-              onChange={(e) => setNumero(Number(e.target.value))}
+              value={numbers}
+              onChange={(e) => setNumbers(Number(e.target.value))}
               InputLabelProps={{
                 shrink: true,
               }}
             />
-            <Button variant="text" onClick={onClickButao}>
+            <Button variant="text" onClick={handleAdd}>
               Add
             </Button>
-            {show && (
-              <div>
-                Hello {text}, Number {numero}
-              </div>
-            )}
 
             <TableContainer component={Paper}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -68,6 +97,7 @@ export default function Forms() {
                   <TableRow>
                     <TableCell>Text</TableCell>
                     <TableCell align="right">Number</TableCell>
+                    <TableCell align="right">Actions</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -76,10 +106,46 @@ export default function Forms() {
                       key={index}
                       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
-                      <TableCell component="th" scope="row">
-                        {row.text}
-                      </TableCell>
-                      <TableCell align="right">{row.numero}</TableCell>
+                      {editIdx === index ? (
+                        <>
+                          <TableCell component="th" scope="row">
+                            <TextField
+                              value={editText}
+                              onChange={(e) => setEditText(e.target.value)}
+                            />
+                          </TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              value={editNumbers}
+                              onChange={(e) => setEditNumbers(Number(e.target.value))}
+                              type="number"
+                            />
+                          </TableCell>
+                          <TableCell align="right">
+                            <IconButton onClick={() => saveEdit(index)}>
+                              <SaveIcon />
+                            </IconButton>
+                            <IconButton onClick={cancelEdit}>
+                              <CancelIcon />
+                            </IconButton>
+                          </TableCell>
+                        </>
+                      ) : (
+                        <>
+                          <TableCell component="th" scope="row">
+                            {row.text}
+                          </TableCell>
+                          <TableCell align="right">{row.numbers}</TableCell>
+                          <TableCell align="right">
+                            <IconButton onClick={() => startEdit(index, row)}>
+                              <EditIcon />
+                            </IconButton>
+                            <IconButton onClick={() => deleteRow(index)}>
+                              <DeleteIcon />
+                            </IconButton>
+                          </TableCell>
+                        </>
+                      )}
                     </TableRow>
                   ))}
                 </TableBody>
